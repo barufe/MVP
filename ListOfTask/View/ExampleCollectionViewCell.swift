32a28ed1988276ff696c3fa9f298258bf3efa34c
barufe <belkisarufe@gmail.com>
2024-08-22 01:07:10 -0500

    collectionView

---
+++ ListOfTask/View/ExampleCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleCollectionViewCell.swift
+//  ListOfTask
+//
+//  Created by Belkis Arufe on 22/08/24.
+//
+
+import UIKit
+
+class ExampleCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
